bbb1bab52bea10839691e4e24a5496c92acb4e99
Cris <crisvdnolle@hotmail.com>
2019-04-02 13:36:55 +0200

    small changes made to implementation

--- source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
+++ source/ExternalDLL/ExternalDLL/StudentPreProcessing.cpp
@@ -1,8 +1,24 @@
 #include "StudentPreProcessing.h"
+#include "ImageFactory.h"
+#include <vector>
 
 
 IntensityImage * StudentPreProcessing::stepToIntensityImage(const RGBImage &image) const {
-	return nullptr;
+	std::vector<std::vector<RGB>> copy;
+	IntensityImage *newCopy = ImageFactory::newIntensityImage();
+	newCopy->set(image.getWidth(), image.getHeight());
+	for (int i = 0; i < newCopy->getHeight(); i++) {
+		for (int j = 0; j < newCopy->getWidth(); j++) {
+		}
+	}
+	/*copy.resize(image.getHeight(), std::vector<RGB>(image.getWidth()));
+	for (int i = 0; i < image.getHeight(); i++) {
+		for (int j = 0; j < image.getWidth(); j++) {
+			copy[i][j] = image.getPixel(i, j);
+		}
+	}
+	*/
+	return newCopy;
 }
 
 IntensityImage * StudentPreProcessing::stepScaleImage(const IntensityImage &image) const {
